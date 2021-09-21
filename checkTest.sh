e6332aee41a3b4cc0e73e3a968459e50fd6627de
Valarmathy <valarmathy.ramachandran@outlook.com>
2021-09-21 11:51:30 +0530

    Checked whether the entered Number is prime or not and Palindrome or not

--- checkTest.sh
+++ checkTest.sh
@@ -1,21 +1,29 @@
 echo  "Enter the Number :"
 read n
-
-for((i=2;i<=$n/2;i++))
+checkPrime() {
+for((i=2;i<=n/2;i++))
 do
 	ans=$(( n%i ))
 	if [ $ans -eq 0 ]
 	then
-		echo "$n is not a prime number";
-		exit 0
+		echo "$n is not a prime number"
+		checkPalindrome;
+	exit
+	fi
+	done
+		echo "$n is a prime number"
+}
+
+checkPalindrome() {
+
 s=0
 rev=""
 temp=$n
 while [ $n -gt 0 ]
 do
 	s=$(( $n % 10 ))
-	n=$(( $n / 10 ))
-rev=$( echo ${rev}${s} )
+	n=$(( $n /10 ))
+	rev=$( echo ${rev}${s} )
 done
 
 	if [ $temp -eq $rev ]
@@ -25,12 +33,13 @@ done
    echo "Number is NOT palindrome"
 	fi
 
-else
+}
 
-echo "$n is a prime number"
 
-fi
-done
+checkPrime;
+
+
+
 
 
 
